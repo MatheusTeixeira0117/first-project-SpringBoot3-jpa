ad8daa0ff2b25d3a275d78073d641df188fbddf1
Matheus Teixeira <matheus_teixeira2506@outlook.com>
2025-08-01 20:52:58 -0300

    Project created

---
+++ src/main/java/com/mydomini/courseJava/CourseJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mydomini.courseJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydomini/courseJava/CourseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydomini.courseJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
