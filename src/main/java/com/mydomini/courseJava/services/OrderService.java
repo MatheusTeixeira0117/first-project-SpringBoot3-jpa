f66a893be88336b8666afd8a077030f225854c3b
Matheus Teixeira <matheus_teixeira2506@outlook.com>
2025-08-07 18:16:50 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/mydomini/courseJava/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.mydomini.courseJava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mydomini.courseJava.entities.Order;
+import com.mydomini.courseJava.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long Id) {
+		Optional<Order> obj = repository.findById(Id); 
+		return obj.get();
+	}
+	
+}
