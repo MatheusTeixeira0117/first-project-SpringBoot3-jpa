cc631e048633330d7b9a1fe185e780675d58414d
Matheus Teixeira <matheus_teixeira2506@outlook.com>
2025-08-08 20:59:26 -0300

    OCategory implementation

---
+++ src/main/java/com/mydomini/courseJava/resources/CategoryResouces.java
@@ -0,0 +1,36 @@
+package com.mydomini.courseJava.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mydomini.courseJava.entities.Category;
+import com.mydomini.courseJava.services.CategoryService;
+
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResouces {
+	
+	@Autowired
+	private CategoryService service;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Category>>  findAll(){
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{Id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/mydomini/courseJava/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.mydomini.courseJava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mydomini.courseJava.entities.Category;
+import com.mydomini.courseJava.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
