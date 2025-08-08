ab9da8c90ab77a4c855ead28540da7faf6a851fc
Matheus Teixeira <matheus_teixeira2506@outlook.com>
2025-08-08 18:36:43 -0300

    OrderStatus enum

--- src/main/java/com/mydomini/courseJava/config/TestConfig.java
+++ src/main/java/com/mydomini/courseJava/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.mydomini.courseJava.entities.Order;
 import com.mydomini.courseJava.entities.User;
+import com.mydomini.courseJava.entities.enums.OrderStatus;
 import com.mydomini.courseJava.repositories.OrderRepository;
 import com.mydomini.courseJava.repositories.UserRepository;
 
@@ -31,11 +32,10 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Larissa", "larissa@gmail.com", "11923557799", "54321");
 		User u3 = new User(null, "Caleri", "caleri@gmail.com", "11923667733", "12354");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u3);
-		Order o3 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
-		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WATING_PAYMENT, u3);
+		Order o3 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WATING_PAYMENT, u2);
+		Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1); 
 		
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
