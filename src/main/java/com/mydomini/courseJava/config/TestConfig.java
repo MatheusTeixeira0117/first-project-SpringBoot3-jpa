34432dd0fa98c9cf1c57a4284ed4ab8dce7e3645
Matheus Teixeira <matheus_teixeira2506@outlook.com>
2025-08-21 18:05:02 -0300

    Payment, one-to-one association

--- src/main/java/com/mydomini/courseJava/config/TestConfig.java
+++ src/main/java/com/mydomini/courseJava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.mydomini.courseJava.entities.Category;
 import com.mydomini.courseJava.entities.Order;
 import com.mydomini.courseJava.entities.OrderItem;
+import com.mydomini.courseJava.entities.Payment;
 import com.mydomini.courseJava.entities.Product;
 import com.mydomini.courseJava.entities.User;
 import com.mydomini.courseJava.entities.enums.OrderStatus;
@@ -87,6 +88,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null , Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		
+		orderRepository.save(o1);
 	}
 	
 	
