23fb57b4761596ac164e859d38708084b71224fd
Matheus Teixeira <matheus_teixeira2506@outlook.com>
2025-08-26 16:27:07 -0300

    Exception handling - findById

--- src/main/java/com/mydomini/courseJava/services/UserService.java
+++ src/main/java/com/mydomini/courseJava/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mydomini.courseJava.entities.User;
 import com.mydomini.courseJava.repositories.UserRepository;
+import com.mydomini.courseJava.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long Id) {
 		Optional<User> obj = repository.findById(Id); 
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));
 	}
 	
 	public User insert(User obj) {
